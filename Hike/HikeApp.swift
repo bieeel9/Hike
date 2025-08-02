5a40ea269acf9610d0e17cef8f9adfb8b817262d
Gabriel <santos.gabrieljesus@outlook.com>
2025-08-02 01:30:21 -0300

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Gabriel Jesus Santos on 02/08/25.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
